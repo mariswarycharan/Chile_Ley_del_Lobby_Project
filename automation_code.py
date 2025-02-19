5c0bb175ca7a3e42e65704017e049f73148ff42f
M.HARISH <harishpmb341@gmail.com>
2025-02-19 14:14:55 +0530

    update in automation script

--- automation_code.py
+++ automation_code.py
@@ -1,4 +1,4 @@
-#import schedule
+import schedule
 import time
 from data_scraping import scrape_data
 from storygeneration import generate_narratives
@@ -9,7 +9,7 @@ def run_script():
     print("Running scheduled script...")
     
     # Step 1 : Scrape data from the website
-    #scrape_data()
+    scrape_data()
     
     # Step 2 : Generate narratives from the scraped data
     generate_narratives()
@@ -18,25 +18,25 @@ def run_script():
     create_vector_store()
     
     print("Script execution completed.")
-run_script()
+
 # Function to schedule the script
-#def schedule_script(day="sunday", exec_time="00:00"):
+def schedule_script(day="sunday", exec_time="00:00"):
     # Clear any existing schedules
-    #schedule.clear()
+    schedule.clear()
     
     # Schedule the script
-    #getattr(schedule.every(), day).at(exec_time).do(run_script)
-    #print(f"Scheduled script to run every {day} at {exec_time}.")
+    getattr(schedule.every(), day).at(exec_time).do(run_script)
+    print(f"Scheduled script to run every {day} at {exec_time}.")
 
 # User input for day and time (Optional)
-#user_day = "Tuesday"
-#user_time = "00:00"
+user_day = "Tuesday"
+user_time = "00:00"
 
 # Schedule the script based on user input
-#schedule_script(user_day, user_time)
+schedule_script(user_day, user_time)
 
 # Keep the script running
-#while True:
-    #schedule.run_pending()
-    #time.sleep(60)  # Check every minute
+while True:
+    schedule.run_pending()
+    time.sleep(60)  # Check every minute
 
