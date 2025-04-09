af85c87dae350e4b319d95797be5661c8aae492f
Charan A A <mariswarycharan@gmail.com>
2025-04-09 11:37:12 +0530

    update in automation code

--- automation_code_run_once.py
+++ automation_code_run_once.py
@@ -1,5 +1,3 @@
-import schedule
-import time
 from data_scraping import scrape_data
 from storygeneration import generate_narratives
 from faiss_db import create_vector_store
@@ -63,24 +61,7 @@ def run_script():
     
     print("Script execution completed.")
 
-# Function to schedule the script
-def schedule_script(day="sunday", exec_time="00:00"):
-    # Clear any existing schedules
-    schedule.clear()
-    
-    # Schedule the script
-    getattr(schedule.every(), day).at(exec_time).do(run_script)
-    print(f"Scheduled script to run every {day} at {exec_time}.")
 
-# User input for day and time (Optional)
-user_day = "Tuesday"
-user_time = "00:00"
-
-# Schedule the script based on user input
-schedule_script(user_day, user_time)
-
-# Keep the script running
-while True:
-    schedule.run_pending()
-    time.sleep(60)  # Check every minute
-
+if __name__ == "__main__":
+    # Run the script
+    run_script()
