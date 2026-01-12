1bfd4e96f8bafd634574d91d7b431964c3fc394c
M.HARISH <harishpmb341@gmail.com>
2026-01-12 14:57:59 +0530

    data updated on 2026-01-12 14:57:58.709078

--- automation_code_run_once.py
+++ automation_code_run_once.py
@@ -49,19 +49,19 @@ def run_script():
     print("Running scheduled script...")
     
     # Step 1 : Scrape data from the website
-    print("Scraping data from the website...")
-    scrape_data()
-    print("Scraping completed.")
-
-    # Step 2 : Generate narratives from the scraped data
-    print("Generating narratives from the scraped data...")
-    generate_narratives()
-    print("Generating narratives completed.")
-    
-    # Step 3 : Create vector store
-    print("Creating vector store...")
-    create_vector_store()
-    print("Vector store creation completed.")
+    # print("Scraping data from the website...")
+    # scrape_data()
+    # print("Scraping completed.")
+    #
+    # # Step 2 : Generate narratives from the scraped data
+    # print("Generating narratives from the scraped data...")
+    # generate_narratives()
+    # print("Generating narratives completed.")
+    #
+    # # Step 3 : Create vector store
+    # print("Creating vector store...")
+    # create_vector_store()
+    # print("Vector store creation completed.")
     
     # Step 4 : push chnages to repository
     print("Pushing changes to repository...")
