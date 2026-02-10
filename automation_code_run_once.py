2dcfa8d88018ae21076fcecb160390b3dd3b68c4
M.HARISH <harishpmb341@gmail.com>
2026-02-10 14:44:03 +0530

    ALL FILES REVERT TO FEB 2 2026

--- automation_code_run_once.py
+++ automation_code_run_once.py
@@ -46,17 +46,17 @@ def update_excel_with_last_updated(timestamp, excel_file="update_log.xlsx", shee
 
 # Function to execute the script
 def run_script():
-    # print("Running scheduled script...")
-    #
-    # # Step 1 : Scrape data from the website
-    # print("Scraping data from the website...")
-    # scrape_data()
-    # print("Scraping completed.")
-    #
-    # # Step 2 : Generate narratives from the scraped data
-    # print("Generating narratives from the scraped data...")
-    # generate_narratives()
-    # print("Generating narratives completed.")
+    print("Running scheduled script...")
+    
+    # Step 1 : Scrape data from the website
+    print("Scraping data from the website...")
+    scrape_data()
+    print("Scraping completed.")
+
+    # Step 2 : Generate narratives from the scraped data
+    print("Generating narratives from the scraped data...")
+    generate_narratives()
+    print("Generating narratives completed.")
 
     # Step 3 : Create vector store
     print("Creating vector store...")
